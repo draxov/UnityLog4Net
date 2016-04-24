6418e4b55597bc6eaff4bfe6b6e866194cecbcdf
Thomas Roberts <draxov42@gmail.com>
2016-04-24 21:41:44 +0100

    Updated log4net for Unity 5
    Added editor tests
    Added example config in resources

---
+++ Assets/Log4Net/Log4NetUnityImpl/Editor/Log4NetTest.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using log4net.Config;
+using UnityEngine;
+using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace log4net.Unity.Tests
+{
+    public class Log4NetTest
+    {
+        private bool m_receivedLogMessage;
+        private bool m_atExpectedLevel;
+        private LogType m_expectedType;
+        private I_IUnityLog m_log;
+
+        [Test]
+        public void TestLog4NetConfig()
+        {
+            const string log4Netconfig = "Log4NetConfig";
+            TextAsset textAsset = Resources.Load<TextAsset>(log4Netconfig);
+            Assert.IsNotNull(textAsset, string.Format("Could not load log4net config at {0}", log4Netconfig));
+            using (Stream stream = GenerateStreamFromString(textAsset.text))
+            {
+                XmlConfigurator.Configure(stream);
+            }
+
+            m_expectedType = LogType.Log;
+
+            m_receivedLogMessage = false;
+            m_log.Debug("Debug");
+            Assert.IsTrue(m_receivedLogMessage, "Did not receive log message");
+            Assert.IsTrue(m_atExpectedLevel, "Log message was recieved at an unexpected level");
+
+            m_receivedLogMessage = false;
+            m_log.Info("Info");
+            Assert.IsTrue(m_receivedLogMessage);
+            Assert.IsTrue(m_atExpectedLevel);
+
+            m_expectedType = LogType.Warning;
+
+            m_receivedLogMessage = false;
+            m_log.Warn("Warn");
+            Assert.IsTrue(m_receivedLogMessage);
+            Assert.IsTrue(m_atExpectedLevel);
+
+            m_expectedType = LogType.Error;
+
+            m_receivedLogMessage = false;
+            m_log.Error("Error");
+            Assert.IsTrue(m_receivedLogMessage);
+            Assert.IsTrue(m_atExpectedLevel);
+
+            m_receivedLogMessage = false;
+            m_log.Fatal("Fatal");
+            Assert.IsTrue(m_receivedLogMessage);
+            Assert.IsTrue(m_atExpectedLevel);
+        }
+
+        [SetUp]
+        public void OnSetUp()
+        {
+            m_log = UnityLogManager.GetLogger(typeof(Log4NetTest));
+            Application.logMessageReceived += OnLogCallback;
+        }
+
+        private void OnLogCallback(string condition, string stacktrace, LogType type)
+        {
+            m_receivedLogMessage = true;
+            m_atExpectedLevel = type == m_expectedType;
+        }
+
+        [TearDown]
+        public void OnTearDown()
+        {
+            LogManager.Shutdown();
+            Application.logMessageReceived -= OnLogCallback;
+        }
+
+        public Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
---
+++ Assets/Log4Net/Log4NetUnityImpl/I_IUnityLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using log4net;
+using Object = UnityEngine.Object;
+
+namespace log4net.Unity
+{
+    public interface I_IUnityLog : ILog
+    {
+        void Debug(string message, Object unityObject, Exception exception);
+        void Debug(string message, Object unityObject);
+        void Info(string message, Object unityObject, Exception exception);
+        void Info(string message, Object unityObject);
+        void Warn(string message, Object unityObject, Exception exception);
+        void Warn(string message, Object unityObject);
+        void Error(string message, Object unityObject, Exception exception);
+        void Error(string message, Object unityObject);
+        void Fatal(string message, Object unityObject, Exception exception);
+        void Fatal(string message, Object unityObject);
+    }
+}
